bea7b394561cafb1c898c9711fa049b395b9cffd
Dao Bui <dao-bui@hotmail.com>
2018-12-20 15:58:35 +0100

    Refactored code, partly implemented validation

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createSwitchNavigator, createStackNavigator, createAppContainer } from 
 import LoadingScreen from './screens/LoadingScreen';
 import WelcomeScreen from './screens/WelcomScreen';
 import StartGameScreen from './screens/StartGameScreen';
+import StatementScreen from './screens/StatementScreen';
 
 const AppStack = createStackNavigator({ home: WelcomeScreen })
 
